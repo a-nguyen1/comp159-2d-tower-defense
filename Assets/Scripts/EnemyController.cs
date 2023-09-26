3635ea9e2c8f1573dbe5ba02efb6b1de0c9782ec
L0Pope <l_pope1@u.pacific.edu>
2023-09-25 22:59:54 -0700

    Made enemy controller script nicer

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,38 +8,23 @@ using Vector2 = UnityEngine.Vector2;
 public class EnemyController : MonoBehaviour
 {
     [SerializeField] private GameObject enemy;
-    //[SerializeField] private GameObject dome;
-    [SerializeField] private float enemySpeed = 0.25f;
-    [SerializeField] private Vector2 domeLocation;
+    [SerializeField] private float enemySpeed;
     private GameObject player;
     
     // Start is called before the first frame update
     void Start()
     {
-        domeLocation = new Vector2(1,1);
         player = GameObject.FindGameObjectWithTag("Player");
-        //StartMoving();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(enemySpeed);
-        Vector2 playerLocation = player.GetComponent<Transform>().position; 
-        //Vector2 thisLocation = enemy.GetComponent<Transform>().position; 
-        Vector2 rangeLocation = enemy.transform.InverseTransformPoint(player.transform.position);
-        Rigidbody2D enemyBody = enemy.GetComponent<Rigidbody2D>();
-        //enemyBody.AddForce(this.transform.forward);
-        enemyBody.AddForce(rangeLocation.normalized*enemySpeed); 
-    }
-
-    void StartMoving()
-    {
-        Debug.Log(enemySpeed);
-        Rigidbody2D enemyBody = enemy.GetComponent<Rigidbody2D>();
-        enemyBody.AddForce(this.transform.forward);
-        //enemyBody.AddForce(domeLocation*enemySpeed);
-        //Debug.Log(domeLocation);
+        Vector2 rangeLocation = enemy.transform.InverseTransformPoint(player.transform.position); //Gets location of dome relative to enemyObject
+        
+        Rigidbody2D enemyBody = enemy.GetComponent<Rigidbody2D>(); //Grabs rigidbody from enemy to move it
+        
+        enemyBody.AddForce(rangeLocation.normalized*enemySpeed); //Moves enemy towards dome
     }
     
 }
