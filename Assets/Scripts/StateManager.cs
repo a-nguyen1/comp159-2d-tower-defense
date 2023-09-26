f18bb071d18c3e6845a7ec2ea0551631982fb140
g-gonza <walerisdev@gmail.com>
2023-09-26 00:27:41 -0700

    Rough state machine for game loop

---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    public static StateManager Instance;
+    public GameState State;
+
+    public static event Action<GameState> OnGameStateChanged;
+
+    void Awake(){
+        Instance = this;
+    }
+    
+    //These states can call other scripts, but don't put much else here.
+    //It would be best to have other scripts listen for these events.
+
+    public void UpdateState(GameState state){
+        Debug.Log("State changted from " + State.ToString() + " to " + state.ToString());
+        State = state;
+        switch(state){
+            case GameState.Setup:
+                Debug.Log("Setup");
+                break;
+            case GameState.Intermission:
+
+                break;
+            case GameState.Round_Start:
+
+                break;
+            case GameState.Round_End:
+
+                break;
+            case GameState.Game_Over:
+
+                break;
+            default:
+                Debug.LogError("Invalid state change");
+                break;
+        }
+
+        OnGameStateChanged?.Invoke(state);
+    }
+    void Start()
+    {
+        UpdateState(GameState.Setup);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum GameState{
+    Setup,
+    Intermission,
+    Round_Start,
+    Round_End,
+    Game_Over,
+}
