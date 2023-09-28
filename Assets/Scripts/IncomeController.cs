5dbc4bd7150e88e3e599f7947a34e9cc00f3d4cf
Vince212 <v_vontersch@u.pacific.edu>
2023-09-27 22:32:00 -0700

    added simple functions to increase bank total when round ends or enemy is defeated

--- Assets/Scripts/IncomeController.cs
+++ Assets/Scripts/IncomeController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class IncomeController : MonoBehaviour
 {
+    [SerializeField] private int bankTotal = 100;
+    [SerializeField] private int enemyGold = 5;
+
+    [SerializeField] private int roundGold = 25;
     // Start is called before the first frame update
+    public void EnemyDown()
+    {
+        bankTotal += enemyGold;
+    }
+
+    public void RoundEndGold()
+    {
+        bankTotal += roundGold;
+    }
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //Use this to check that when calling above functions, banktotal is properly updating
+        Debug.Log( bankTotal);
     }
 }
