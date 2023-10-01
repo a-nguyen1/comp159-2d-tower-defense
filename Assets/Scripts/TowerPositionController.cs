3941163f32be05dfbe5f5019b823179f283860f7
Austin Nguyen <a_nguyen202@u.pacific.edu>
2023-09-30 19:49:14 -0700

    change buy button label to buy/sell accordingly

--- Assets/Scripts/TowerPositionController.cs
+++ Assets/Scripts/TowerPositionController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -26,17 +27,29 @@ public class TowerPositionController : MonoBehaviour
     //TODO disable circle collider when round starts and enable during setup/intermission
     public void OnMouseDown()
     {
-        if (!towerExists) // TODO check if player has enough currency to buy/upgrade tower
+        GameObject child = gameObject.transform.GetChild(0).gameObject; // canvas
+        child = child.transform.GetChild(1).gameObject; // buy button
+        child = child.transform.GetChild(0).gameObject; // buy button text
+        TextMeshProUGUI textLabel = child.GetComponent<TextMeshProUGUI>(); // buy button text label
+        if (!towerExists) // TODO check if player has enough currency to buy tower
         {
             // TODO add UI to buy tower
             currentTower = Instantiate(tower, transform.localPosition, Quaternion.identity);
             towerExists = true;
+            if (textLabel.text.Contains("Buy Turret"))
+            {
+                textLabel.SetText("Sell Turret");
+            }
         }
         else
         {
+            // TODO add money when selling tower
+            if (textLabel.text.Contains("Sell Turret"))
+            {
+                textLabel.SetText("Buy Turret");
+            }
             Destroy(currentTower);
             towerExists = false;
-            // TODO add UI for upgrading or selling tower
         }
     }
 
